221ffe102c849c70d6918237a15b944f8d5247ab
Lex Alexander <l.alexander10@gmail.com>
2024-11-20 18:40:30 -0800

    Scafoold  grievance report view

--- healthwatch_ai_app/models/grievance_report.py
+++ healthwatch_ai_app/models/grievance_report.py
@@ -3,25 +3,7 @@ from django.core.validators import MinLengthValidator
 from enum import Enum
 from django.utils import timezone
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
-import re
-import pdb
-from datetime import timedelta
-from ..ml_models.similar_medical_request_analyzer import SimilarMedicalRequestAnalyzer
 
-
-class Duration(Enum):
-  LESS_THAN_A_DAY = "Less Than A Day"
-  ONE_TO_THREE_DAYS = "1 - 3 Days"
-  FOUR_TO_SEVEN_DAYS = "4 - 7 Days"
-  ONE_TO_FOUR_WEEKS = "1 - 4 Weeks"
-  MORE_THAN_A_MONTH = "More than a month"
-
-class MedicalRequestSeverity(Enum):
-  HIGH = 'High'
-  MEDIUM = 'Medium'
-  MODERATE = 'Moderate'
-  LOW = 'Low'
 
 class GrievanceReport(models.Model):
   created_at = models.DateTimeField(default=timezone.now)
